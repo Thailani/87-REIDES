d1d46c7e36dc6f35bbf01887254fa5a9e4cf77d2
Thailani <111435891+Thailani@users.noreply.github.com>
2023-03-02 18:16:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage)
+{
+	fabric.Image.fromURL(get_image, function(Img) {
+	block_image_object = Img;
+	
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_height);
+	block_image_object.set({
+	top:block_y,
+	left:block_x
+	});
+	canvas.add(block_image_object);
+    });
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '?') // adicione os códigos adequados às teclas
+	{
+		blockX = 150;
+		new_image('prback.jpg');
+		console.log("e");
+	}
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		new_image('gr.png');
+		console.log("v");
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		new_image('yr.png');
+		console.log("a");
+	}
+
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		new_image('rr1.png');
+		console.log("r");
+	}
+
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+		new_image('br.png');
+		console.log("i");
+	}
+	
+}
+
